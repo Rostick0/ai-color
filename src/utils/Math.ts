0e22452a14ca33e770c4e1e9eea4aba3e5446c9a
Rostislav <rostik057@gmail.com>
2025-07-26 19:38:56 +0300

    init

---
+++ src/utils/Math.ts
@@ -0,0 +1,26 @@
+const Matrix = {
+  fromArray: (arr: number[]): number[][] => arr.map((i) => [i]),
+
+  getSize: (a: number[][]): [number, number] => [a[0].length, a.length],
+
+  sum: (a: number[][], b: number[][]): number[][] =>
+    a.map((row, i) => row.map((val, j) => val + b[i][j])),
+
+  scale: (a: number[][], scalar: number): number[][] =>
+    a.map((row) => row.map((val) => val * scalar)),
+
+  mul: <T = number[][]>(a: number[][], b: number[][]): T => {
+    const [aCols, aRows] = Matrix.getSize(a);
+    const [bCols, bRows] = Matrix.getSize(b);
+
+    if (aCols !== bRows) {
+      throw new Error("Matrix size mismatch: a.cols !== b.rows");
+    }
+
+    return Array.from({ length: aRows }, (_, i) =>
+      Array.from({ length: bCols }, (_, j) =>
+        a[i].reduce((sum, val, k) => sum + val * b[k][j], 0)
+      )
+    ) as T;
+  },
+};
